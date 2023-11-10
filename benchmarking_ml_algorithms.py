88627c2831ccec9da31df48141c642ba8e6f7d9f
Jarne Vermant <96956881+vermantjarne@users.noreply.github.com>
2023-11-10 20:28:29 +0100

    Add files via upload

--- benchmarking_ml_algorithms.py
+++ benchmarking_ml_algorithms.py
@@ -13,15 +13,8 @@ import streamlit as st
 st.title("Task 2 - Benchmarking ML Algorithms")
 
 # Store initial variable values in session state
-if "visibility" not in st.session_state:
+if "selection" not in st.session_state:
     st.session_state.selection = "Decision Trees"
-
-# Request user input
-st.radio(
-    "Select machine learning algorithm",
-    ["Decision Trees", "Logistic Regression", "Support Vector Machine"],
-    key="selection"
-)
 
 # Fetch dataset
 dataset_car_evaluation = fetch_ucirepo(id=19)
@@ -33,8 +26,7 @@ y = dataset_car_evaluation.data.targets
 # Print data analysis
 st.write(f"For this example, we are using the [Car Evaluation dataset](https://archive.ics.uci.edu/dataset/19/car+evaluation)")
 st.write()
-st.write(f"The dataset has {str(len(X.columns))} features")
-st.write(f"The dataset has {str(len(X))} instances")
+st.write(f"The dataset has {str(len(X.columns))} features and {str(len(X))} instances")
 st.write()
 st.write("The dataset has the following first 5 rows:")
 st.write(X.head())
@@ -52,16 +44,19 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 X_train_encoded = pd.get_dummies(X_train)
 X_test_encoded = pd.get_dummies(X_test)
 
-# Create model depending on the chosen algorithm
+# Create model depending on the chosen algorithm and fit data into model
 if st.session_state.selection == "Decision Trees":
     model = DecisionTreeClassifier()
+    model.fit(X_train_encoded, y_train)
 elif st.session_state.selection == "Logistic Regression":
     model = LogisticRegression()
+    model.fit(X_train_encoded, np.ravel(y_train))
 else:
     model = SVC()
+    model.fit(X_train_encoded, np.ravel(y_train))
 
 # Fit train data into model
-model.fit(X_train_encoded, np.ravel(y_train))
+#model.fit(X_train_encoded, np.ravel(y_train))
 
 # Create predictions for model based on test data
 predictions = model.predict(X_test_encoded)
@@ -69,5 +64,12 @@ predictions = model.predict(X_test_encoded)
 # Determine accuracy for predictions based on test data
 accuracy = accuracy_score(y_test, predictions)
 
+# Request user input for choosing 
+st.radio(
+    "Select machine learning algorithm",
+    ["Decision Trees", "Logistic Regression", "Support Vector Machine"],
+    key="selection"
+)
+
 # Write accuracy to streamlit application
-st.write(f"The accuracy for this model is {np.round(accuracy * 100, 2)}")
+st.write(f"The accuracy for the {st.session_state.selection} model is {np.round(accuracy * 100, 2)}")
